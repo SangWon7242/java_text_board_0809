7deb2e8948f09feb50a242f111e72b9ade31244a
SangWon7242 <gotkdqja@gmail.com>
2024-08-14 16:46:45 +0900

    리스트 두개를 이용하여 데이터 분석하는 방법의 단점

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,7 +3,9 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    String queryString = "b=20&c=30&d=40&a=100";
+    String queryString = "hit=73&id=3&memberId=13&boardId=2";
+
+    // 압축해제 시작
     String[] queryStringBits = queryString.split("&");
 
     List<String> paramNames = new ArrayList<>();
@@ -17,14 +19,11 @@ public class Main {
       paramNames.add(paramName);
       paramValues.add(Integer.parseInt(paramValue));
     }
+    // 압축해제 끝
 
-    for(int i = 0; i < paramNames.size(); i++) {
-      String paramName = paramNames.get(i);
-      int paramValue = paramValues.get(i);
-
-      System.out.printf("%s : %d\n", paramName, paramValue);
-    }
-
+    System.out.println(paramNames);
+    int boardIdIndex = paramNames.indexOf("boardId");
+    System.out.println("boardId : " + paramValues.get(boardIdIndex));
 
   }
 }
