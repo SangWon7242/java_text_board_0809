d0723a949cbe1ace3e0cb40cc8cd32be4292ee9d
SangWon7242 <gotkdqja@gmail.com>
2024-08-14 16:19:49 +0900

    파싱로직에 유연하지 못한 문제를 파악

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,13 +1,14 @@
-import java.util.Arrays;
-
 public class Main {
   public static void main(String[] args) {
-    String queryString = "a=100&b=20&c=30";
+    // 문제점 : 현재 우리 파싱로직은 유연하지 못한다.
+    // 왜? 파라미터 추가되면 추가 된 내용에 대해서 추가 작업을 해줘야 하기 때문에...
+    String queryString = "a=100&b=20&c=30&d=40";
     String[] queryStringBits = queryString.split("&");
 
     int a = 0;
     int b = 0;
     int c = 0;
+    int d = 0;
 
     for(String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
@@ -23,6 +24,9 @@ public class Main {
       else if(paramName.equals("c")) {
         c = Integer.parseInt(paramValue);
       }
+      else if(paramName.equals("d")) {
+        d = Integer.parseInt(paramValue);
+      }
 
       // System.out.printf("%s : %s\n", paramName, paramValue);
     }
@@ -30,6 +34,7 @@ public class Main {
     System.out.printf("a : %d\n", a);
     System.out.printf("b : %d\n", b);
     System.out.printf("c : %d\n", c);
+    System.out.printf("d : %d\n", d);
 
   }
 }
