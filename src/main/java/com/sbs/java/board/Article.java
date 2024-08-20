4d2a8ec836067268f4ebaba72e356d01606a9090
SangWon7242 <gotkdqja@gmail.com>
2024-08-20 17:48:03 +0900

    클래스별 파일 분리

---
+++ src/main/java/com/sbs/java/board/Article.java
@@ -0,0 +1,18 @@
+package com.sbs.java.board;
+
+public class Article {
+  int id;
+  String subject;
+  String content;
+
+  Article(int id, String subject, String content) {
+    this.id = id;
+    this.subject = subject;
+    this.content = content;
+  }
+
+  @Override // 어노테이션
+  public String toString() { // 메서드 오버라이딩
+    return "{id : %d, subject: \"%s\", content : \"%s\"}".formatted(id, subject, content);
+  }
+}
