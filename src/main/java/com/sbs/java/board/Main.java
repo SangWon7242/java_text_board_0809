ca5872755ede694a0404db1a1b296a989e1cc08c
SangWon7242 <gotkdqja@gmail.com>
2024-08-20 16:06:17 +0900

    usr/article/list를 별도의 메서드로 분리

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -6,7 +6,7 @@ import java.util.stream.IntStream;
 public class Main {
   static void makeTestData(List<Article> articles) {
     IntStream.rangeClosed(1, 100)
-        .forEach(i -> articles.add(new Article(i, "제목" + i, "내용1" + i)));
+        .forEach(i -> articles.add(new Article(i, "제목" + i, "내용" + i)));
   }
 
   public static void main(String[] args) {
@@ -72,51 +72,7 @@ public class Main {
         System.out.printf("내용 : %s\n", article.content);
 
       } else if (rq.getUrlPath().equals("/usr/article/list")) {
-        Map<String, String> params = rq.getParams();
-
-        if (articles.isEmpty()) {
-          System.out.println("게시물이 존재하지 않습니다.");
-          continue;
-        }
-
-        boolean orderByIdDesc = true;
-
-        if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
-          orderByIdDesc = false;
-        }
-
-        // 검색 시작
-        List<Article> filteredArticles = articles;
-
-        if (params.containsKey("searchKeyword")) {
-          String searchKeyword = params.get("searchKeyword");
-
-          filteredArticles = new ArrayList<>();
-
-          for (Article article : articles) {
-            if (article.subject.contains(searchKeyword) || article.content.contains(searchKeyword)) {
-              filteredArticles.add(article);
-            }
-          }
-        }
-        // 검색 끝
-
-        List<Article> soredArticles = filteredArticles;
-        // List<Article> soredArticles = articles;
-
-        System.out.println("== 게시물 리스트 ==");
-        System.out.println("-------------------");
-        System.out.println("|  번호  |  제목  |");
-        System.out.println("-------------------");
-
-        if (orderByIdDesc) { // idAsc(오름차순)가 없으면 기본값인 idDesc(내림차순)
-          soredArticles = Util.reverseList(soredArticles);
-        }
-
-        for (Article article : soredArticles) {
-          System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
-        }
-
+        actionUsrArticleList(rq, articles);
       } else if (rq.getUrlPath().equals("exit")) {
         break;
       } else {
@@ -126,6 +82,53 @@ public class Main {
 
     System.out.println("== 자바 텍스트 게시판 종료 ==");
     sc.close();
+  }
+
+  static void actionUsrArticleList(Rq rq, List<Article> articles) {
+    Map<String, String> params = rq.getParams();
+
+    if (articles.isEmpty()) {
+      System.out.println("게시물이 존재하지 않습니다.");
+      return;
+    }
+
+    boolean orderByIdDesc = true;
+
+    if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
+      orderByIdDesc = false;
+    }
+
+    // 검색 시작
+    List<Article> filteredArticles = articles;
+
+    if (params.containsKey("searchKeyword")) {
+      String searchKeyword = params.get("searchKeyword");
+
+      filteredArticles = new ArrayList<>();
+
+      for (Article article : articles) {
+        if (article.subject.contains(searchKeyword) || article.content.contains(searchKeyword)) {
+          filteredArticles.add(article);
+        }
+      }
+    }
+    // 검색 끝
+
+    List<Article> soredArticles = filteredArticles;
+    // List<Article> soredArticles = articles;
+
+    System.out.println("== 게시물 리스트 ==");
+    System.out.println("-------------------");
+    System.out.println("|  번호  |  제목  |");
+    System.out.println("-------------------");
+
+    if (orderByIdDesc) { // idAsc(오름차순)가 없으면 기본값인 idDesc(내림차순)
+      soredArticles = Util.reverseList(soredArticles);
+    }
+
+    for (Article article : soredArticles) {
+      System.out.printf("|   %d    |  %s  |\n", article.id, article.subject);
+    }
   }
 }
 
